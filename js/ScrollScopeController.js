b8565c6652fcc17ede99f8d9df27c8e0652369d8
HIJIKIsw <hijiki_seaweed@yahoo.co.jp>
2017-10-09 20:34:09 +0900

    ・SideBar の data-scroll-scope 属性をインライン化 ・Tasks をリデザイン ・disabled 属性が付いている Checkbox の色を薄いグレーに変更 　クリック時の Ripple も無効に

--- js/ScrollScopeController.js
+++ js/ScrollScopeController.js
@@ -5,8 +5,8 @@
 	$(document).ready(function(){
 		var AttrName = 'data-scroll-scope';
 
-		$('#SideBar').attr(AttrName, '');
 		$('.minict_wrapper > ul').attr(AttrName, '');
+
 		$(this).scrollScope({
 			elements: '['+AttrName+']',
 		});
