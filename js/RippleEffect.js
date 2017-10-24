0f86fdc62c3f8f7c8a286ae90268eba277abd285
HIJIKIsw <hijiki_seaweed@yahoo.co.jp>
2017-10-24 14:46:31 +0900

    ・Colors テーブルを Material Design のカラーパレットに基づいたものに修正 ・Color 関数で不透明度を指定できるように 　それに伴い各コンポーネントでの Color 関数使用箇所を修正 ・検索バーのサイズを変数化 ・CSS で実装していた Ripple Effect を一旦削除 ・その他細かな微調整

--- js/RippleEffect.js
+++ js/RippleEffect.js
@@ -2,29 +2,29 @@
 //= RippleEffect.js
 //--------------------------------------------------------------------------------------------------
 
-	$(document).ready(function()
-	{
-		$('.Ripple-White').each(function()
-		{
-			$(this).click(function()
-			{
-				SpawnRipple(this);
-			});
-		});
+	// $(document).ready(function()
+	// {
+	// 	$('.Ripple-White').each(function()
+	// 	{
+	// 		$(this).click(function()
+	// 		{
+	// 			SpawnRipple(this);
+	// 		});
+	// 	});
 
-		function SpawnRipple(Parent)
-		{
-			var Size = Math.max($(Parent).outerWidth(), $(Parent).outerHeight());
-			console.log(Size);
-			var RippleEffect = $('<div class="RippleEffect"></div>').appendTo(Parent);
-			RippleEffect.css({
-				'width': Size,
-				'height': Size,
-			});
+	// 	function SpawnRipple(Parent)
+	// 	{
+	// 		var Size = Math.max($(Parent).outerWidth(), $(Parent).outerHeight());
+	// 		console.log(Size);
+	// 		var RippleEffect = $('<div class="RippleEffect"></div>').appendTo(Parent);
+	// 		RippleEffect.css({
+	// 			'width': Size,
+	// 			'height': Size,
+	// 		});
 
-			setTimeout(function ()
-			{
-				$(RippleEffect).remove();
-			}, 1000);
-		}
-	});
+	// 		setTimeout(function ()
+	// 		{
+	// 			$(RippleEffect).remove();
+	// 		}, 1000);
+	// 	}
+	// });
